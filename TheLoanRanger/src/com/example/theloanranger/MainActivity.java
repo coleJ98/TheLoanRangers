aeba4e079e778293c58f9cf3e45ae8993a4d40f9
Mitchell Rice <ricemitc@uga.edu>
2014-10-23 12:58:10 -0400

    Trying to fix my repo

--- TheLoanRanger/src/com/example/theloanranger/MainActivity.java
+++ TheLoanRanger/src/com/example/theloanranger/MainActivity.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,36 +15,29 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //ActionBar bar = getActionBar();
-        //bar.setBackgroundDrawable(new ColorDrawable(R.color.off_white_background));
-        //bar.setDisplayShowTitleEnabled(false);
-        //bar.setDisplayShowTitleEnabled(true);
-
+       
+        
+        //Trying to fix my repo with this comment
+        
         
         setContentView(R.layout.activity_main);
         
+        
         Button calculate = (Button) findViewById(R.id.calculate);
-        calculate.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent openPage = new Intent(getBaseContext(), DisplayActivity.class);
-				startActivity(openPage);
+               calculate.setOnClickListener(new View.OnClickListener() {
+        			
+        			@Override
+        			public void onClick(View v) {
+        				// TODO Auto-generated method stub
+        				Intent openPage = new Intent(getBaseContext(), DisplayActivity.class);
+        				startActivity(openPage);
+        
+        			}
+        		});
+             
+         
+        
 
-			}
-		});
-        
-        Spinner selectMonth = (Spinner)findViewById(R.id.month_spinner);
-        String[] months = new String[]{"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
-        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, months);
-        selectMonth.setAdapter(monthAdapter);
-        
-        Spinner selectYear = (Spinner)findViewById(R.id.year_spinner);
-        //****TODO:     Fill in the rest of the years later - maybe 30 year max?
-        String[] years = new String[]{"2014", "2015", "2016", "2017", "..."};
-        ArrayAdapter<String> yearAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, years);
-        selectYear.setAdapter(yearAdapter); 
     }
 
 
